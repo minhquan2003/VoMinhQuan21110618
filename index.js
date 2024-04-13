35a66d792640bbf601ac43d293ac8eaac562e349
minhquan2003 <105582848+minhquan2003@users.noreply.github.com>
2024-04-13 09:50:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import StopWatch from './StopWatch';
 
-AppRegistry.registerComponent(appName, () => StopWatch);
+AppRegistry.registerComponent(appName, () => App);
